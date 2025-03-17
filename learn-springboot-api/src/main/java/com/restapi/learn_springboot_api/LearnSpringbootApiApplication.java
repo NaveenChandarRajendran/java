8360ea02c35c4ce903fd24edc346ebc417d085af
Naveen <naveen.ndr97@gmail.com>
2025-03-17 07:37:01 +0530

    Learn : Started rest api

---
+++ learn-springboot-api/src/main/java/com/restapi/learn_springboot_api/LearnSpringbootApiApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.learn_springboot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringbootApiApplication.class, args);
+	}
+
+}
---
+++ learn-springboot-api/src/test/java/com/restapi/learn_springboot_api/LearnSpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.learn_springboot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
