b8cbbd8a76c04846724d1db5f1975a3be7dc833f
Naveen <naveen.ndr97@gmail.com>
2025-03-26 06:30:21 +0530

    Learn : Enhancing post method and user not found error

--- learn-springboot-api/src/main/java/com/restapi/learn_springboot_api/user/UserDaoService.java
+++ learn-springboot-api/src/main/java/com/restapi/learn_springboot_api/user/UserDaoService.java
@@ -25,12 +25,13 @@ public class UserDaoService {
 	
 	public User findOne(int id) {
 		Predicate<? super User> predicate = user -> user.getId().equals(id); 
-		return users.stream().filter(predicate).findFirst().get();
+		return users.stream().filter(predicate).findFirst().orElse(null);
 	}
 	
-	public void save(User user) {
+	public User save(User user) {
 		user.setId(++userCount);
 		users.add(user);
+		return user;
 	}
 
 }
