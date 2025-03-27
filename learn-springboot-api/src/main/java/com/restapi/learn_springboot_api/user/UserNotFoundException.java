117ff6ff8475d37a62007d7d77c0452bf60c6204
Naveen <naveen.ndr97@gmail.com>
2025-03-27 06:59:18 +0530

    Learned : Custom error showing in API fail

--- learn-springboot-api/src/main/java/com/restapi/learn_springboot_api/user/UserNotFoundException.java
+++ learn-springboot-api/src/main/java/com/restapi/learn_springboot_api/user/UserNotFoundException.java
@@ -3,9 +3,10 @@ package com.restapi.learn_springboot_api.user;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@SuppressWarnings("serial")
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
-	public UserNotFoundException(String message) {
+	public UserNotFoundException(String message, int id) {
 		super(message);
 	}
 }
