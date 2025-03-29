110ee426286c7ea4a788df84b92320c3fd1f370e
Naveen <naveen.ndr97@gmail.com>
2025-03-29 21:47:31 +0530

    Learned : Error handle

--- learn-springboot-api/src/main/java/com/restapi/learn_springboot_api/exception/CustomizedResponseEntityExceptionHandler.java
+++ learn-springboot-api/src/main/java/com/restapi/learn_springboot_api/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.restapi.learn_springboot_api.exception;
 
+import java.net.http.HttpHeaders;
 import java.time.LocalDate;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -28,6 +31,13 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
 		
+	}
+	
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), 
+				"Total Errors:" + ex.getErrorCount() + " First Error:" + ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+		return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
 	}
 
 }
--- learn-springboot-api/src/main/java/com/restapi/learn_springboot_api/user/User.java
+++ learn-springboot-api/src/main/java/com/restapi/learn_springboot_api/user/User.java
@@ -2,8 +2,12 @@ package com.restapi.learn_springboot_api.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Size;
+
 public class User {
 	private Integer id;
+	
+	@Size(min = 2,message = "Name should have at least 2 charectors")
 	private String name;
 	private LocalDate birthLocalDate;
 	
